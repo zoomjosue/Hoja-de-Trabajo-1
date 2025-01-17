01c9057db996f1bb88abf3bf2a097a3cd907d768
Josué García <gar24918@uvg.edu.gt>
2025-01-17 12:26:57 -0600

    Creación de la clase interface IBlender

---
+++ IBlender.java
@@ -0,0 +1,9 @@
+public interface IBlender{
+    public void SpeedUp();
+    public boolean IsFull();
+    public void Fill();
+    public void Empty();
+    public void SpeedDown();
+    public int GetSpeed();
+    
+}
